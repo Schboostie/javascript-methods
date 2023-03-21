b9fd85e833a8ed24113492c8a17dcc75a2157f52
Shahid Amin CYF <82290321+Schboostie@users.noreply.github.com>
2023-03-21 00:19:31 +0000

    Create stringMethods.js

---
+++ stringMethods.js
@@ -0,0 +1,11 @@
+const str1 = "Hello";
+const str2 = "world!";
+const result = str1 + " " + str2;
+console.log(result); // Output: "Hello world!"
+
+
+/*In the above example, we define two string variables str1 and str2. 
+Then we use the + operator to concatenate them along with a space character between them to create a new string called result. 
+Finally, we print out the concatenated string using console.log().*/
+
+
