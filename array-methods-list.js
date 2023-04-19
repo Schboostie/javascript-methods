2b7ce275b48af3cb5594f396a7125c06713484c7
Shahid Amin CYF <82290321+Schboostie@users.noreply.github.com>
2023-04-19 10:21:05 +0100

    Create array-methods-list.js

---
+++ array-methods-list.js
@@ -0,0 +1,25 @@
+//Twenty common array methods to explore...
+
+/*
+concat(): Joins two or more arrays and returns a new array.
+filter(): Creates a new array with all elements that pass the test implemented by the provided function.
+find(): Returns the value of the first element in the array that satisfies the provided testing function.
+forEach(): Calls a function for each element in the array.
+includes(): Determines whether an array includes a certain value among its entries, returning true or false.
+indexOf(): Returns the first index at which a given element can be found in the array, or -1 if it is not present.
+join(): Joins all elements of an array into a string.
+lastIndexOf(): Returns the last index at which a given element can be found in the array, or -1 if it is not present.
+map(): Creates a new array with the results of calling a provided function on every element in the array.
+pop(): Removes the last element from an array and returns that element.
+push(): Adds one or more elements to the end of an array and returns the new length of the array.
+reduce(): Applies a function against an accumulator and each element in the array to reduce it to a single value.
+reverse(): Reverses the order of the elements in an array in place.
+shift(): Removes the first element from an array and returns that element.
+slice(): Returns a shallow copy of a portion of an array into a new array object selected from start to end.
+some(): Tests whether at least one element in the array passes the test implemented by the provided function.
+sort(): Sorts the elements of an array in place and returns the sorted array.
+splice(): Changes the contents of an array by removing or replacing existing elements and/or adding new elements.
+`toString()
+
+
+*/
